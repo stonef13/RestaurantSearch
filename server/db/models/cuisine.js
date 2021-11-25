0fcad565cdb75ea538e22a567394020080b59d91
Stone Fuglaar <fuglaarusa@gmail.com>
2021-11-24 16:51:22 -0800

    Feature: cuisine model

---
+++ server/db/models/cuisine.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Cuisine = db.define('cuisine', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  }
+})
