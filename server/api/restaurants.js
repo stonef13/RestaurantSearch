8454fe38eebb063ad2aa700427e82550b30ad8c8
Stone Fuglaar <fuglaarusa@gmail.com>
2021-11-26 20:06:27 -0800

    Feature: attachment of Cuisine to Restaurant Objects within API route

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
 const Restaurant = require('../db/models/restaurant')
+const Cuisine = require('../db/models/cuisine')
 
 router.get('/', async (req, res, next) => {
   try {
-    const allRestaurants = await Restaurant.findAll()
+    const allRestaurants = await Restaurant.findAll({
+      include: [
+        {
+          model: Cuisine
+        }
+      ]
+    })
     res.json(allRestaurants)
   } catch (err) {
     console.error(err)
