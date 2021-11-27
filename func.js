ff78a12e417d4952b83711e83ac849acdebdcae8
Stone Fuglaar <fuglaarusa@gmail.com>
2021-11-26 19:36:31 -0800

    Feature: static restaurant search function

--- func.js
+++ func.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-statements */
+/* eslint-disable complexity */
+
 const {restaurants} = require('./funcData')
 
 // const restaurants = [
@@ -30,6 +33,10 @@ function restaurantSearch(name, rating, distance, price, cuisine) {
   const searchResults = []
   //exact match for name
   if (name) {
+    if (typeof name !== 'string') {
+      console.error('Name is not a String!')
+      return
+    }
     for (let i = 0; i < restaurants.length; i++) {
       if (name === restaurants[i].name) {
         searchResults.push(restaurants[i])
@@ -38,6 +45,10 @@ function restaurantSearch(name, rating, distance, price, cuisine) {
   }
 
   if (rating) {
+    if (typeof rating !== 'number') {
+      console.error('rating is not an Integer!')
+      return
+    }
     for (let i = 0; i < restaurants.length; i++) {
       if (restaurants[i].customer_rating >= rating) {
         if (searchResults.includes(restaurants[i])) {
@@ -49,9 +60,45 @@ function restaurantSearch(name, rating, distance, price, cuisine) {
     }
   }
 
+  if (distance) {
+    if (typeof distance !== 'number') {
+      console.error('distance is not an Integer!')
+      return
+    }
+    for (let i = 0; i < restaurants.length; i++) {
+      if (restaurants[i].distance <= distance) {
+        if (searchResults.includes(restaurants[i])) {
+          continue
+        } else {
+          searchResults.push(restaurants[i])
+        }
+      }
+    }
+  }
+
+  if (price) {
+    if (typeof price !== 'number') {
+      console.error('Price is not an Integer!')
+      return
+    }
+    for (let i = 0; i < restaurants.length; i++) {
+      if (restaurants[i].price <= price) {
+        if (searchResults.includes(restaurants[i])) {
+          continue
+        } else {
+          searchResults.push(restaurants[i])
+        }
+      }
+    }
+  }
+
+  //STILL NEED CUISINES BUT WAIT FOR POSTGRES
+
+  //SORT METHOD TO SORT RESTAURANTS BY DISTANCE
+
   return searchResults
 }
 
-console.log(restaurantSearch('Herbed Delicious', 5, null, null, null))
+console.log(restaurantSearch(null, null, null, 10, null))
 
-//DATA
+//SORT METHOD TO SORT RESTAURANTS BY DISTANCE
